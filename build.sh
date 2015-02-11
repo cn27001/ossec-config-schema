07b832bb66209d0d06f8599d55bc168f74b3f66f
Robin cabrel Harper <cabrel@zerklabs.com>
2015-02-11 11:35:01 -0500

    added linux/386 to build script

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 printf "** Building linux/amd64\n"
-go build -a github.com/sedarasecurity/ossec/types
-go install github.com/sedarasecurity/ossec/types
+go build -a github.com/sedarasecurity/ossec-config-schema
+go install github.com/sedarasecurity/ossec-config-schema
+
+printf "** Building linux/386\n"
+go-linux-386 build -a github.com/sedarasecurity/ossec-config-schema
+go-linux-386 install github.com/sedarasecurity/ossec-config-schema
 
 printf "** Building windows/386\n"
-go-windows-386 build -a github.com/sedarasecurity/ossec/types
-go-windows-386 install github.com/sedarasecurity/ossec/types
+go-windows-386 build -a github.com/sedarasecurity/ossec-config-schema
+go-windows-386 install github.com/sedarasecurity/ossec-config-schema
 
 printf "** Building windows/amd64\n"
-go-windows-amd64 build -a github.com/sedarasecurity/ossec/types
-go-windows-amd64 install github.com/sedarasecurity/ossec/types
+go-windows-amd64 build -a github.com/sedarasecurity/ossec-config-schema
+go-windows-amd64 install github.com/sedarasecurity/ossec-config-schema
