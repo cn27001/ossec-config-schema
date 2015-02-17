48fed5e817454052407374fef26fb15634e81acd
Robin cabrel Harper <cabrel@zerklabs.com>
2015-02-17 10:35:21 -0500

    Added omitempty to the ignore structs for the Type attribute

--- ignore.go
+++ ignore.go
@@ -4,6 +4,6 @@ import "encoding/xml"
 
 type Ignore struct {
 	XMLName xml.Name `xml:"ignore"`
-	Type    string   `xml:"type,attr"`
+	Type    string   `xml:"type,attr,omitempty"`
 	Value   string   `xml:",chardata"`
 }
--- registry_ignore.go
+++ registry_ignore.go
@@ -4,6 +4,6 @@ import "encoding/xml"
 
 type RegistryIgnore struct {
 	XMLName xml.Name `xml:"registry_ignore"`
-	Type    string   `xml:"type,attr"`
+	Type    string   `xml:"type,attr,omitempty"`
 	Value   string   `xml:",chardata"`
 }
